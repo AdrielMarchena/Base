0a6c93d3c1228f695f6c0764c93adccfcbcab81e
AdrielMarchena <Adriel_contato@hotmail.com>
2021-05-25 21:02:57 -0300

    Change pointer to Texture to a const reference to Texture

--- Base/src/render/SubTexture.cpp
+++ Base/src/render/SubTexture.cpp
@@ -13,27 +13,20 @@ namespace en
 {
 namespace render
 {
-	static render::Texture default_white_texture = render::Texture();
-	SubTexture::SubTexture(const Texture* texture, const glm::vec2& min, const glm::vec2& max)
+	static const render::Texture default_white_texture = render::Texture();
+	SubTexture::SubTexture(const Texture& texture, const glm::vec2& min, const glm::vec2& max)
 		:m_Texture(texture)
 	{
-		if (m_Texture && m_Texture->GetId())
+		if (m_Texture.GetId())
 		{
 			m_TexCoords[0] = { min.x,min.y };
 			m_TexCoords[1] = { max.x,min.y };
 			m_TexCoords[2] = { max.x,max.y };
 			m_TexCoords[3] = { min.x,max.y };
 		}
-		else
-		{
-			//Anoying cout, just return nullptr
-			//std::cout << "Texture does not exist!" << std::endl;
-			m_Texture = nullptr;
-		}
-
 	}
 
-	SubTexture SubTexture::CreateFromCoords(const Texture* texture, const glm::vec2 size, const glm::vec2& coords, const glm::vec2& spriteSize)
+	SubTexture SubTexture::CreateFromCoords(const Texture& texture, const glm::vec2 size, const glm::vec2& coords, const glm::vec2& spriteSize)
 	{
 		float sheetWidth = size.x, sheetHeight = size.y;
 
@@ -45,13 +38,11 @@ namespace render
 
 	GLuint SubTexture::GetId() const
 	{
-		if (!m_Texture || !m_Texture->GetId())
+		if (!m_Texture.GetId())
 		{
-			//Anoying cout, just return NULL
-			//std::cout << "Texture does not exist!" << std::endl;
 			return NULL;
 		}
-		return m_Texture->GetId();
+		return m_Texture.GetId();
 	}
 }
 }
--- Base/src/render/SubTexture.h
+++ Base/src/render/SubTexture.h
@@ -16,12 +16,12 @@ namespace render
 	class SubTexture
 	{
 	private:
-		const Texture* m_Texture = nullptr;
+		const Texture& m_Texture;
 		glm::vec2 m_TexCoords[4] = { glm::vec2(0.0f) };
 	public:
-		SubTexture() {}
-		SubTexture(const Texture* texture, const glm::vec2& pos, const glm::vec2& size);
-		static SubTexture CreateFromCoords(const Texture* texture, const glm::vec2 size, const glm::vec2& coords, const glm::vec2& spriteSize);
+		SubTexture();
+		SubTexture(const Texture& texture, const glm::vec2& pos, const glm::vec2& size);
+		static SubTexture CreateFromCoords(const Texture& texture, const glm::vec2 size, const glm::vec2& coords, const glm::vec2& spriteSize);
 		GLuint GetId() const;
 		/* 4 */
 		const glm::vec2* GetTexCoords() const { return m_TexCoords; };
