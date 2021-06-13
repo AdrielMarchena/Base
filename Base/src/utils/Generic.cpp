890d8e8a018c33789d5d6b8e3a364f3d11224d46
AdrielMarchena <Adriel_contato@hotmail.com>
2021-06-13 20:08:24 -0300

    Add Generic funtions file

---
+++ Base/src/utils/Generic.cpp
@@ -0,0 +1,36 @@
+#include "Generic.h"
+
+namespace en
+{
+namespace utils
+{
+	std::string ToUpper(const std::string& str)
+	{
+		std::string tmp = "";
+		for (auto& c : str)
+		{
+			if (c >= 'a' && c <= 'z')
+			{
+				tmp += c - 32;
+			}
+			else
+				tmp += c;
+		}
+		return tmp;
+	}
+	std::string ToLower(const std::string& str)
+	{
+		std::string tmp = "";
+		for (auto& c : str)
+		{
+			if (c >= 'A' && c <= 'Z')
+			{
+				tmp += c + 32;
+			}
+			else
+				tmp += c;
+		}
+		return tmp;
+	}
+}
+}
---
+++ Base/src/utils/Generic.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+namespace en
+{
+namespace utils
+{
+	std::string ToUpper(const std::string& str);
+	std::string ToLower(const std::string& str);
+}
+}
