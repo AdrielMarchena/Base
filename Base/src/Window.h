7d1e42649f8c01a350873604e6610e11d211c367
AdrielMarchena <Adriel_contato@hotmail.com>
2021-07-16 21:56:20 -0300

    Add some comments on some methods for intelliSense things

--- Base/src/Window.h
+++ Base/src/Window.h
@@ -8,6 +8,7 @@
 #pragma once
 #include <math.h>
 #include <memory>
+#include <algorithm>
 #include <string>
 #include "gl/glew.h"
 #include "GLFW/glfw3.h"
@@ -62,7 +63,6 @@ namespace en
 			ALCcontext* p_ALCContext = nullptr;
 
 		public:
-			//Constructors
 			Window(const char* title, float_t w = 800, float_t h = 600, bool resizeble = true);
 			~Window();
 
@@ -71,39 +71,59 @@ namespace en
 			Window& operator=(Window& other) = delete;
 			Window& operator=(Window&& other) noexcept;
 
+			//Called before the Game Loop start
 			virtual void OnAttach(AttachArgs args);
+			//Called on the loop, before OnRender
 			virtual void OnUpdate(UpdateArgs args);
+			//Called on the loop
 			virtual void OnRender(RenderArgs args);
+			//Last thing called on the Loop
 			virtual void OnImGui(ImGuiArgs args);
+			//Called after the Game Loop ends
 			virtual void Dispose();
 
 			void SetResizeble(bool resizeble);
 
+			//Initializa the Game Loop
 			void Run();
 
 		private:
+			//Used as callback in glfw
 			static void on_resize(GLFWwindow* window, int32_t w, int32_t h);
+			//Used as callback in glfw
 			static void on_mouse_scroll(GLFWwindow* window, double_t xOffSet, double_t yOffSet);
+			//Used as callback in glfw
 			static void on_cursor_move(GLFWwindow* window, double_t xPos, double_t yPos);
+			//Used as callback in glfw
 			static void on_mouse_button(GLFWwindow* window, int32_t key, int32_t action, int32_t mods);
+			//Used as callback in glfw
 			static void on_keyboard_button(GLFWwindow* window, int32_t key, int32_t scancode, int32_t action, int32_t mods);
+			//Used as callback in glfw
 			static void on_mouse_enter(GLFWwindow* window, int32_t entered);
 		protected:
+			// Return the mouse position with the y axis inverted
 			glm::vec2 correct_mouse(input::Mouse& mouse)
 			{
 				return { mouse.gpos().x, -(mouse.gpos().y - m_Hei) };
 			}
 
+			//Return a correted position of the cursor, taking in consideration the camera position
 			glm::vec2 m_pos(input::Mouse& mouse)
 			{
 				glm::vec2 mpos = correct_mouse(mouse);
-				return mpos += glm::vec2(m_camera.GetCamera().GetPosition().x, m_camera.GetCamera().GetPosition().y) * (glm::vec2(m_Wid, m_Hei) / 2.0f);
+				mpos += glm::vec2(m_camera.GetCamera().GetPosition().x, m_camera.GetCamera().GetPosition().y) * (glm::vec2(m_Wid, m_Hei) / 2.0f);
+				//mpos.x = std::clamp(mpos.x,0.0f,m_Wid);
+				//mpos.y = std::clamp(mpos.y,0.0f,m_Hei);
+				return mpos;
 			}
 			void inline SetPerpectiveInShaders();
 			void inline SetViewInShaders();
 			void inline SetTransformInShaders();
 
+			//Hide the cursor on the window
+			//Whitout removing the funtionality
 			void HideCursor();
+			//Unhide the cursor
 			void UnhideCursor();
 
 			//Return true if the mouse is hovering the window
