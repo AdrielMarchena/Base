dacc30cd9a1f78df280b905451a57fe0d454e0bd
AdrielMarchena <adriel_contato@hotmail.com>
2021-08-31 23:41:58 -0300

    Static class now

--- Base/src/render/render2D.cpp
+++ Base/src/render/render2D.cpp
@@ -11,12 +11,22 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "gl/glew.h"
 #include <iostream>
+
+#include "geo_renders/QuadRender.h"
+#include "geo_renders/LineRender.h"
+#include "geo_renders/CircleRender.h"
+#include "geo_renders/TriRender.h"
+
 namespace en
 {
 namespace render
 {
 	glm::vec3 Render2D::m_default_axis = { 0.0f,0.0f,1.0f };
-	
+	static std::shared_ptr<QuadRender2D> m_QuadRender;
+	static std::shared_ptr<CircleRender> m_CircleRender;
+	static std::shared_ptr<LineRender2D> m_LineRender;
+	static std::shared_ptr<QuadRender2D> m_TextRender;
+	static std::shared_ptr<TriRender> m_TriRender;
 
 	void Render2D::SetClearColor(const glm::vec4& color)
 	{
@@ -33,111 +43,104 @@ namespace render
 		GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 	}
 
-	Render2D::Render2D(const char* quad_vs, const char* quad_fs,
-		const char* line_vs, const char* line_fs,
-		const char* circle_vs, const char* circle_fs, 
-		const char* text_vs, const char* text_fs,
-		const char* tri_vs, const char* tri_fs)
-		:m_QuadRender(quad_vs,quad_fs), 
-		m_LineRender(line_vs,line_fs),
-		m_CircleRender(circle_vs,circle_fs),
-		m_TextRender(text_vs,text_fs),
-		m_TriRender(tri_vs,tri_fs)
+	void Render2D::Init(const char* quad_vs, const char* quad_fs, const char* line_vs, const char* line_fs, const char* circle_vs, const char* circle_fs, const char* text_vs, const char* text_fs, const char* tri_vs, const char* tri_fs)
 	{
+		m_QuadRender = std::make_shared<QuadRender2D>(quad_vs, quad_fs);
+		m_LineRender = std::make_shared<LineRender2D>(line_vs, line_fs);
+		m_CircleRender = std::make_shared<CircleRender>(circle_vs, circle_fs);
+		m_TextRender = std::make_shared<QuadRender2D>(text_vs, text_fs);
+		m_TriRender = std::make_shared<TriRender>(tri_vs, tri_fs);
+
 		GLCall(glEnable(GL_DEPTH_TEST));
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glEnable(GL_MULTISAMPLE));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 	}
-
-	Render2D::~Render2D()
-	{
-	}
 	
 	void Render2D::BeginBatch()
 	{
-		m_LineRender.BeginBatch();
-		m_QuadRender.BeginBatch();
-		m_CircleRender.BeginBatch();
-		m_TextRender.BeginBatch();
-		m_TriRender.BeginBatch();
+		m_LineRender->BeginBatch();
+		m_QuadRender->BeginBatch();
+		m_CircleRender->BeginBatch();
+		m_TextRender->BeginBatch();
+		m_TriRender->BeginBatch();
 	}
 
 	void Render2D::EndBatch()
 	{
-		m_LineRender.EndBatch();
-		m_QuadRender.EndBatch();
-		m_CircleRender.EndBatch();
-		m_TextRender.EndBatch();
-		m_TriRender.EndBatch();
+		m_LineRender->EndBatch();
+		m_QuadRender->EndBatch();
+		m_CircleRender->EndBatch();
+		m_TextRender->EndBatch();
+		m_TriRender->EndBatch();
 	}
 
 	void Render2D::Flush()
 	{
-		m_LineRender.Flush();
-		m_QuadRender.Flush();
-		m_CircleRender.Flush();
-		m_TextRender.Flush();
-		m_TriRender.Flush();
+		m_LineRender->Flush();
+		m_QuadRender->Flush();
+		m_CircleRender->Flush();
+		m_TextRender->Flush();
+		m_TriRender->Flush();
 	}
 
 	void Render2D::Dispose()
 	{
-		m_QuadRender.Dispose();
-		m_CircleRender.Dispose();
-		m_LineRender.Dispose();
-		m_TextRender.Dispose();
-		m_TriRender.Dispose();
+		m_QuadRender->Dispose();
+		m_CircleRender->Dispose();
+		m_LineRender->Dispose();
+		m_TextRender->Dispose();
+		m_TriRender->Dispose();
 	}
 
 	const Shader& Render2D::GetQuadShader()
 	{
-		return m_QuadRender.GetShader();
+		return m_QuadRender->GetShader();
 	}
 
 	const Shader& Render2D::GetLineShader()
 	{
-		return m_LineRender.GetShader();
+		return m_LineRender->GetShader();
 	}
 
 	const Shader& Render2D::GetCircleShader()
 	{
-		return m_CircleRender.GetShader();
+		return m_CircleRender->GetShader();
 	}
 
 	const Shader& Render2D::GetTextShader()
 	{
-		return m_TextRender.GetShader();
+		return m_TextRender->GetShader();
 	}
 
 	const Shader& Render2D::GetTriShader()
 	{
-		return m_TriRender.GetShader();
+		return m_TriRender->GetShader();
 	}
 
 	void Render2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color, float_t layer, float_t rotation, const glm::vec3& axis)
 	{
-		m_QuadRender.DrawQuad(position, size, color, layer, rotation, axis);
+		m_QuadRender->DrawQuad(position, size, color, layer, rotation, axis);
 	}
 
 	void Render2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4 color[4], float_t layer, float_t rotation, const glm::vec3& axis)
 	{
-		m_QuadRender.DrawQuad(position, size, color, layer, rotation, axis);
+		m_QuadRender->DrawQuad(position, size, color, layer, rotation, axis);
 	}
 
 	void Render2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Texture& texture, float_t layer, const glm::vec4& color,float_t rotation, const glm::vec3& axis)
 	{
-		m_QuadRender.DrawQuad(position, size, texture, color, layer, rotation, axis);
+		m_QuadRender->DrawQuad(position, size, texture, color, layer, rotation, axis);
 	}
 
 	void Render2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const SubTexture& sub_texture, float_t layer, float_t rotation, const glm::vec3& axis)
 	{
-		m_QuadRender.DrawQuad(position, size, sub_texture, layer, rotation, axis);
+		m_QuadRender->DrawQuad(position, size, sub_texture, layer, rotation, axis);
 	}
 
 	void Render2D::DrawText(const glm::vec2& position, const glm::vec2& size, const Texture& texture, float_t layer, const glm::vec4& color, float_t rotation, const glm::vec3& axis)
 	{
-		m_TextRender.DrawQuad(position, size, texture, color, layer, rotation, axis);
+		m_TextRender->DrawQuad(position, size, texture, color, layer, rotation, axis);
 	}
 
 	void Render2D::DrawOutLineQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color,
@@ -170,113 +173,113 @@ namespace render
 
 	void Render2D::DrawLine(const glm::vec2& origin, const glm::vec2& dest, const glm::vec4& color, float_t layer)
 	{
-		m_LineRender.DrawLine(origin, dest, color, layer);
+		m_LineRender->DrawLine(origin, dest, color, layer);
 	}
 
 	void Render2D::DrawCurveLine(const glm::vec2& origin, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& dest, const glm::vec4& color, float_t precision ,float_t layer)
 	{
-		m_LineRender.DrawCurveLine(origin, p1, p2, dest,color,precision,layer);
+		m_LineRender->DrawCurveLine(origin, p1, p2, dest,color,precision,layer);
 	}
 
 	void Render2D::DrawCurveLine(const glm::vec2& origin, const glm::vec2& p1, const glm::vec2& dest, const glm::vec4& color, float_t precision, float_t layer)
 	{
-		m_LineRender.DrawCurveLine(origin, p1, dest, color, precision, layer);
+		m_LineRender->DrawCurveLine(origin, p1, dest, color, precision, layer);
 	}
 
 	void Render2D::DrawCircle(const glm::vec2& position, float_t radius, const glm::vec4& color, bool fill, float thick,float_t layer, float_t rotation, const glm::vec3& axis)
 	{
-		m_CircleRender.DrawCircle(position, radius, fill, thick ,color, layer, rotation, axis);
+		m_CircleRender->DrawCircle(position, radius, fill, thick ,color, layer, rotation, axis);
 	}
 
 	void Render2D::DrawTriangle(const glm::vec2 points[3], const glm::vec4& color, float_t layer)
 	{
-		m_TriRender.DrawTriangle(points, color, layer);
+		m_TriRender->DrawTriangle(points, color, layer);
 	}
 
 	void Render2D::DrawTriangle(const glm::vec2 points[3], const glm::vec4 color[3], float_t layer)
 	{
-		m_TriRender.DrawTriangle(points, color, layer);
+		m_TriRender->DrawTriangle(points, color, layer);
 	}
 
 	void Render2D::LineBeginBatch()
 	{
-		m_LineRender.BeginBatch();
+		m_LineRender->BeginBatch();
 	}
 
 	void Render2D::LineEndBatch()
 	{
-		m_LineRender.EndBatch();
+		m_LineRender->EndBatch();
 	}
 
 	void Render2D::LineFlush()
 	{
-		m_LineRender.Flush();
+		m_LineRender->Flush();
 	}
 
 	void Render2D::QuadBeginBatch()
 	{
-		m_QuadRender.BeginBatch();
+		m_QuadRender->BeginBatch();
 	}
 
 	void Render2D::QuadEndBatch()
 	{
-		m_QuadRender.EndBatch();
+		m_QuadRender->EndBatch();
 	}
 
 	void Render2D::QuadFlush()
 	{
-		m_QuadRender.Flush();
+		m_QuadRender->Flush();
 	}
 
 	void Render2D::CircleBeginBatch()
 	{
-		m_CircleRender.BeginBatch();
+		m_CircleRender->BeginBatch();
 	}
 
 	void Render2D::CircleEndBatch()
 	{
-		m_CircleRender.EndBatch();
+		m_CircleRender->EndBatch();
 	}
 
 	void Render2D::CircleFlush()
 	{
-		m_CircleRender.Flush();
+		m_CircleRender->Flush();
 	}
 
 	void Render2D::TextBeginBatch()
 	{
-		m_TextRender.BeginBatch();
+		m_TextRender->BeginBatch();
 	}
 
 	void Render2D::TextEndBatch()
 	{
-		m_TextRender.EndBatch();
+		m_TextRender->EndBatch();
 	}
 
 	void Render2D::TextFlush()
 	{
-		m_TextRender.Flush();
+		m_TextRender->Flush();
 	}
 
 	void Render2D::TriBeginBatch()
 	{
-		m_TriRender.BeginBatch();
+		m_TriRender->BeginBatch();
 	}
 
 	void Render2D::TriEndBatch()
 	{
-		m_TriRender.EndBatch();
+		m_TriRender->EndBatch();
 	}
 
 	void Render2D::TriFlush()
 	{
-		m_TriRender.Flush();
+		m_TriRender->Flush();
 	}
 
 	//Deprecated
 	void Render2D::DrawQuadLine(const glm::vec2& origin, const glm::vec2& dest, const glm::vec4& color, float_t thick, float_t layer)
 	{
-		m_QuadRender.DrawQuadLine(origin, dest, color, thick, layer);
+		m_QuadRender->DrawQuadLine(origin, dest, color, thick, layer);
 	}
 }
 }
--- Base/src/render/render2D.h
+++ Base/src/render/render2D.h
@@ -12,12 +12,7 @@
 #include "SubTexture.h"
 #include <memory>
 #include <math.h>
-
-#include "geo_renders/QuadRender.h"
-#include "geo_renders/LineRender.h"
-#include "geo_renders/CircleRender.h"
-#include "geo_renders/TriRender.h"
-
+#include "Colors.h"
 namespace en
 {
 namespace render
@@ -26,11 +21,13 @@ namespace render
 	class Render2D
 	{
 	private:
-		QuadRender2D m_QuadRender;
-		CircleRender m_CircleRender;
-		LineRender2D m_LineRender;
-		QuadRender2D m_TextRender;
-		TriRender m_TriRender;
+		/* //On the render2D.cpp file
+		static std::shared_ptr<QuadRender2D> m_QuadRender;
+		static std::shared_ptr<CircleRender> m_CircleRender;
+		static std::shared_ptr<LineRender2D> m_LineRender;
+		static std::shared_ptr<QuadRender2D> m_TextRender;
+		static std::shared_ptr<TriRender> m_TriRender;
+		*/
 	public:
 
 		static glm::vec3 m_default_axis;
@@ -39,82 +36,86 @@ namespace render
 		static void ClearColor();
 		static void ClearColorDepth();
 
-		Render2D(const char* quad_vs, const char* quad_fs,
+		/*Render2D(const char* quad_vs, const char* quad_fs,
 				 const char* line_vs, const char* line_fs,
 				 const char* circle_vs, const char* circle_fs,
 				 const char* text_vs, const char* text_fs,
-				 const char* tri_vs, const char* tri_fs);
+				 const char* tri_vs, const char* tri_fs);*/
 		//Render2D(){}
 
-		~Render2D();
+		static void Init(const char* quad_vs, const char* quad_fs,
+			const char* line_vs, const char* line_fs,
+			const char* circle_vs, const char* circle_fs,
+			const char* text_vs, const char* text_fs,
+			const char* tri_vs, const char* tri_fs);
 
-		void BeginBatch();
-		void EndBatch();
-		void Flush();
-		void Dispose();
+		static void BeginBatch();
+		static void EndBatch();
+		static void Flush();
+		static void Dispose();
 
-		const Shader& GetQuadShader();
-		const Shader& GetLineShader();
-		const Shader& GetCircleShader();
-		const Shader& GetTextShader();
-		const Shader& GetTriShader();
+		static const Shader& GetQuadShader();
+		static const Shader& GetLineShader();
+		static const Shader& GetCircleShader();
+		static const Shader& GetTextShader();
+		static const Shader& GetTriShader();
 
-		void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color,
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color,
 			float_t layer = 0, float_t rotation = NULL, const glm::vec3& axis = m_default_axis);
 		
-		void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4 color[4],
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4 color[4],
 			float_t layer = 0, float_t rotation = NULL, const glm::vec3& axis = m_default_axis);
 
-		void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Texture& texture,
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Texture& texture,
 			float_t layer = 0, const glm::vec4& color = Color::White,float_t rotation = NULL, const glm::vec3& axis = m_default_axis);
 
-		void DrawQuad(const glm::vec2& position, const glm::vec2& size, const SubTexture& sub_texture,
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const SubTexture& sub_texture,
 			float_t layer = 0, float_t rotation = NULL, const glm::vec3& axis = m_default_axis);
 
-		void DrawText(const glm::vec2& position, const glm::vec2& size, const Texture& texture,
+		static void DrawText(const glm::vec2& position, const glm::vec2& size, const Texture& texture,
 			float_t layer = 0, const glm::vec4& color = Color::White, float_t rotation = NULL, const glm::vec3& axis = m_default_axis);
 
-		void DrawOutLineQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color,
+		static void DrawOutLineQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color,
 			float_t layer = 0, float_t rotation = NULL, const glm::vec3& axis = m_default_axis);
 
-		void DrawLine(const glm::vec2& origin, const glm::vec2& dest, const glm::vec4& color, float_t layer = 0);
+		static void DrawLine(const glm::vec2& origin, const glm::vec2& dest, const glm::vec4& color, float_t layer = 0);
 
-		void DrawCurveLine(const glm::vec2& origin, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& dest, const glm::vec4& color, float_t precision = 0.01f ,float_t layer = 0);
+		static void DrawCurveLine(const glm::vec2& origin, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& dest, const glm::vec4& color, float_t precision = 0.01f ,float_t layer = 0);
 
-		void DrawCurveLine(const glm::vec2& origin, const glm::vec2& p1, const glm::vec2& dest, const glm::vec4& color, float_t precision = 0.01f, float_t layer = 0);
+		static void DrawCurveLine(const glm::vec2& origin, const glm::vec2& p1, const glm::vec2& dest, const glm::vec4& color, float_t precision = 0.01f, float_t layer = 0);
 
-		void DrawCircle(const glm::vec2& position, float_t radius, const glm::vec4& color, bool fill = true, float thick = 1.0f,
+		static void DrawCircle(const glm::vec2& position, float_t radius, const glm::vec4& color, bool fill = true, float thick = 1.0f,
 			float_t layer = 0, float_t rotation = NULL, const glm::vec3& axis = {});
 
-		void DrawTriangle(const glm::vec2 points[3], const glm::vec4& color, float_t layer);
+		static void DrawTriangle(const glm::vec2 points[3], const glm::vec4& color, float_t layer);
 
-		void DrawTriangle(const glm::vec2 points[3], const glm::vec4 color[3], float_t layer);
+		static void DrawTriangle(const glm::vec2 points[3], const glm::vec4 color[3], float_t layer);
 
 	private:
 
-		void QuadBeginBatch();
-		void QuadEndBatch();
-		void QuadFlush();
+		static void QuadBeginBatch();
+		static void QuadEndBatch();
+		static void QuadFlush();
 
-		void CircleBeginBatch();
-		void CircleEndBatch();
-		void CircleFlush();
+		static void CircleBeginBatch();
+		static void CircleEndBatch();
+		static void CircleFlush();
 
-		void LineBeginBatch();
-		void LineEndBatch();
-		void LineFlush();
+		static void LineBeginBatch();
+		static void LineEndBatch();
+		static void LineFlush();
 
-		void TextBeginBatch();
-		void TextEndBatch();
-		void TextFlush();
+		static void TextBeginBatch();
+		static void TextEndBatch();
+		static void TextFlush();
 
-		void TriBeginBatch();
-		void TriEndBatch();
-		void TriFlush();
+		static void TriBeginBatch();
+		static void TriEndBatch();
+		static void TriFlush();
 
 	public: //deprecated
 		[[deprecated("Use DrawLine() instead")]]
-		void DrawQuadLine(const glm::vec2& origin, const glm::vec2& dest, const glm::vec4& color, float_t thick,
+		static void DrawQuadLine(const glm::vec2& origin, const glm::vec2& dest, const glm::vec4& color, float_t thick,
 			float_t layer = 0.0f);
 	};
 }
