480d717b39364a20788cc2d86aecf74c42666be0
AdrielMarchena <adriel_contato@hotmail.com>
2021-10-22 23:27:28 -0300

    Add Life project

--- premake5.lua
+++ premake5.lua
@@ -153,3 +153,67 @@ project "Sandbox"
 
 	filter { "system:windows", "configurations:Dist" }
 		buildoptions "/MT"
+
+
+
+project "Life"
+	location "Life"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.c",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Base/vendor/stb_image",
+		"Base/vendor/entt/single_include",
+		"Base/vendor/libsndfile/include",
+		"Base/vendor/spdlog/include",
+		"Base/src",
+		"Base/vendor/glm",
+		"Base/vendor/debugbreak",
+	}
+
+	links
+	{
+		"Base"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BASE_WINDOWS_BUILD"
+		}
+
+	filter "configurations:Debug"
+		defines "BASE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BASE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BASE_DIST"
+		optimize "On"
+
+	filter { "system:windows", "configurations:Debug" }
+		buildoptions "/MTd"
+	
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
+
+	filter { "system:windows", "configurations:Dist" }
+		buildoptions "/MT"
